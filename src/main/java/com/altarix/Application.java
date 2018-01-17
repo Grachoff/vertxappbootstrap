b116363e3ec64bf55109dba846a9d1e04ef3218f
grachoff <sergey.grachov@altarix.ru>
2018-01-17 18:48:08 +0300

    improving backbone

--- src/main/java/com/altarix/Application.java
+++ src/main/java/com/altarix/Application.java
@@ -3,7 +3,10 @@ package com.altarix;
 import com.altarix.httpserver.MainHttpServerHandler;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Application extends AbstractVerticle {
     @Override
     public void start(Future<Void> startFuture) throws Exception {
@@ -18,4 +21,25 @@ public class Application extends AbstractVerticle {
                     }
                  });
     }
-}
+
+    public static void main(String[] args) {
+        try {
+            Vertx vertx = Vertx.vertx();
+            Application instance = new Application();
+            vertx.deployVerticle(instance);
+        } catch (Exception e) {
+//            log.error("Exception in launcher", e);
+        }
+    } // END main
+    @Override
+    public void stop() throws Exception {
+        try {
+            // TODO do some stuff, detach consumers, stop timers
+
+            // Removing all verticles
+            vertx.deploymentIDs().forEach(deploymentId -> vertx.undeploy(deploymentId));
+        } catch (Exception e) {
+//            log.error("Exception in Launcher.stop", e);
+        }
+    } // END stop
+} // END Launcher﻿
--- src/main/java/com/altarix/httpserver/MainHttpServerHandler.java
+++ src/main/java/com/altarix/httpserver/MainHttpServerHandler.java
@@ -2,10 +2,13 @@ package com.altarix.httpserver;
 
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MainHttpServerHandler implements Handler<HttpServerRequest> {
     @Override
     public void handle(HttpServerRequest httpServerRequest) {
+        httpServerRequest.absoluteURI();
         httpServerRequest.response().end("OK");
     }
 }
